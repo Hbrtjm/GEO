82a880e274345ec92465347d4e131b63e77e1b3b
Hbrtjm <hubi.hey@vp.pl>
2024-10-04 12:38:16 +0200

    Initial commit

---
+++ lab1/1.py
@@ -0,0 +1,36 @@
+from random import random, randint
+from matplotlib.pyplot import plot 
+from time import time
+
+
+def det1(a,b,c):
+    return (())
+
+def det2(a,b,c):
+    return ((a[0]-c[0])*(b[1]-c[1]))-((b[0]-c[0])*(a[1]-c[1]))
+
+points = [ (random()*randint(-1000,1000),random()*randint(-1000,1000)) for _ in range(10**5) ]
+
+start = time()
+for i in range()det()
+
+
+def calculate(generator,n,generator_name = "None",a=(-1.0,0.0),b=(1.0,1.0)):
+    points = generator()
+    for i in range(n):
+        
+def time_it(genertor_list)
+    for generator_function,generator_name in generator_list:
+        start = time()
+        results = calculate(points)
+        visualize(results)
+        end = time()
+        print (f"For {generator_name} elapsed time {end-start}")
+
+
+functions = [(First_generator,"10 ^ 5 from (-1000,1000)"), (Second_generator,"10 ^ 5 from (-10^14,10^14)"),(Third_generator,"")]
+
+
+time_it()
+
+
