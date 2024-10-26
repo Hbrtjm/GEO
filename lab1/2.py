a113e176cee53b7796e7a36c02b96dfb2661c55d
Hbrtjm <hubi.hey@vp.pl>
2024-10-26 20:06:56 +0200

    Finshed lab 1, working on lab 2

--- lab1/2.py
+++ lab1/2.py
@@ -50,7 +50,7 @@ def decode_colors(orient):
         res.append(cols[orient[i]])
     return res
 
-def calculate(points=[],n=100,epsilon=0,a=(-1.0,0.0),b=(1.0,0.1),detFun=det3x3,dot_size=1,generator_name="generated.tex"):
+def calculate(points=[],n=100,epsilon=0,a=(-1.0,0.0),b=(1.0,0.1),detFun=det3x3,dot_size=1,generator_name="generated.tex",det_name=""):
     states = [0,0,0]
     orient = [ None for _ in range(n) ]
     for i in range(n):
@@ -60,7 +60,7 @@ def calculate(points=[],n=100,epsilon=0,a=(-1.0,0.0),b=(1.0,0.1),detFun=det3x3,d
     decoded = decode_colors(orient)
     plt.scatter(*z,c=decoded,s=[SIZES[dot_size] for _ in range(n)])
     plt.axline(a, b, markersize=2)
-    plt.savefig(generator_name + "_marked.png")
+    plt.savefig(f"{generator_name}_{det_name}_marked.png")
     return states
 
 def time_it(generator_list, epsilons, dets):
@@ -69,10 +69,10 @@ def time_it(generator_list, epsilons, dets):
         with open(tex_filename, "w") as f:
             f.write("\\begin{table}[ht]\n")
             f.write("    \\centering\n")
-            f.write(f"    \\caption{{Comparison of Determinant Methods with Generator {generator_name}}}\n")
+            f.write(f"    \\caption{{Tabela różnic czasów obliczeń i tolerancji dla generatora {generator_name.replace("_"," ")}}}\n")
             f.write("    \\begin{tabular}{@{}llccccc@{}}\n")
             f.write("        \\toprule\n")
-            f.write("        Epsilon & Determinant Name & Calculation Time (s) & Points Above & Points Below & Points on Line \\\\ \\midrule\n")
+            f.write("        Tolerancja \\epsilon & Nazwa wyznacznika & Czas obliczeń (s) & Punkty powyżej prostej & Punkty poniżej prostej & Punkty na prostej \\\\ \\midrule\n")
         points = generator_function(n)
         plt.scatter(*zip(*points),s=[SIZES[size] for _ in range(n)])
         plt.savefig(generator_name + ".png")
@@ -81,7 +81,7 @@ def time_it(generator_list, epsilons, dets):
                 f.write(f"        \\multicolumn{{6}}{{c}}{{Epsilon = {epsilon}}} \\\\ \\midrule\n")
             for detFun, detName in dets:
                 start = time()
-                states = calculate(points=points, n=n, epsilon=epsilon, a=(-1.0, 0.0), b=(1.0, 0.1), detFun=detFun, dot_size=size, generator_name=generator_name)
+                states = calculate(points=points, n=n, epsilon=epsilon, a=(-1.0, 0.0), b=(1.0, 0.1), detFun=detFun, dot_size=size, generator_name=generator_name,det_name=detName)
                 end = time()
                 with open(tex_filename, "a") as f:
                     f.write(f"        {epsilon} & {detName} & {end - start:.2f} & {states[1]} & {states[2]} & {states[0]} \\\\\n")
@@ -91,14 +91,13 @@ def time_it(generator_list, epsilons, dets):
             f.write("\\end{table}\n")
 
 epsilons = [0, 10**-14, 10**-12, 10**-10, 10**-8]
-dets = [(det3x3, "First own"), (det2x2, "Second own"), (det_np_2x2, "First np"), (det_np_3x3, "Second np")]
+dets = [(det3x3, "Własny wyznacznik 3x3"), (det2x2, "Własny wyznacznik 2x2"), (det_np_2x2, "Wyznacznik numpy 2x2"), (det_np_3x3, "Wyznacznik numpy 2x2")]
 
 functions = [
-    (First_generator, 10**5, "10^5_from_(-1000,1000)", 2),
-    (Second_generator, 10**5, "10^5_from_(-10^14,10^14)", 2),
-    (Third_generator, 10**3, "Circle_of_radius_100", 1),
-    (Fourth_generator, 10**3, "Line_y=0.05x+0.05", 0)
+    (First_generator, 10**5, "10^5_z_przedziału_(-1000,1000)", 2),
+    (Second_generator, 10**5, "10^5_z_przedziału_(-10^14,10^14)", 2),
+    (Third_generator, 10**3, "koła_o_promieniu_100", 1),
+    (Fourth_generator, 10**3, "Lini_a_b_y=0.05x+0.05", 0)
 ]
 
 time_it(functions, epsilons, dets)
-
---
+++ lab2/convex_hull.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+from random import uniform
+import math
+from time import time
+
+def angle(point):
+    x, y = point
+    return math.atan2(y, x)
+
+def merge(L, R):
+    new = []
+    i = j = 0
+    while i < len(L) and j < len(R):
+        if angle(L[i]) < angle(R[j]):
+            new.append(L[i])
+            i += 1
+        else:
+            new.append(R[j])
+            j += 1
+    new.extend(L[i:])
+    new.extend(R[j:])
+    return new
+
+def radial_sort(points):
+    if len(points) < 2:
+        return points
+    mid = len(points) // 2
+    left = radial_sort(points[:mid])
+    right = radial_sort(points[mid:])
+    return merge(left, right)
+
+def point_generator(n=20,x_max=100,x_min=-100,y_max=100,y_min=-100):
+    return [ (uniform(x_min,x_max),uniform(y_min,y_max)) for _ in range(n) ] 
+
+def det2x2(a,b,c):
+    return ((a[0]-c[0])*(b[1]-c[1]))-((b[0]-c[0])*(a[1]-c[1]))
+
+def convex_hull(points,detFun=det2x2):
+    result = []
+    points = sorted(points, key=lambda p: (p[1], p[0])) 
+    starting_point = points[0]
+    sorted_points = sorted(points[1:], key=lambda p: math.atan2(p[1] - starting_point[1], p[0] - starting_point[0]))
+
+    print("Sorting points")
+    points = radial_sort(points)
+    print(points)
+    # Start building the convex hull
+    result = [starting_point, sorted_points[0]]
+    print("Creating hull")
+    start = time()
+    for point in sorted_points[1:]:
+        while len(result) > 1 and detFun(result[-2], result[-1], point) < 0:
+            result.pop()
+        result.append(point)
+    finish = time()
+
+    print(f"Finished after {finish - start} seconds")
+    print(result)
+    return result
+
+def print_hull(points,hull):
+    plt.scatter(*zip(*points))
+    n = len(hull)
+    for i in range(n):
+        print(f"From: {hull[i]} to {hull[(i+1)%n]}")
+        x_values = [hull[i][0], hull[(i+1) % n][0]]
+        y_values = [hull[i][1], hull[(i+1) % n][1]]
+        
+        # Plot a line between the current point and the next
+        plt.plot(x_values, y_values, 'r-')
+    plt.show()
+
+points = point_generator(n=100)
+print_hull(points,convex_hull(points))
+
